fbd5fc62a9bf980e0da0875aa6d701d418f428c6
zzz <17704904@qq.com>
2022-02-23 16:32:06 +0800

    deploy

---
+++ precache-manifest.03279b803f37f7bbcdc7cd901e215a91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709f26a91930208251e3",
+    "url": "/HelloAccount/css/app.9194db15.css"
+  },
+  {
+    "revision": "7efb1b4b78cff2876212e8ea7308b0ac",
+    "url": "/HelloAccount/index.html"
+  },
+  {
+    "revision": "709f26a91930208251e3",
+    "url": "/HelloAccount/js/app.cea46c49.js"
+  },
+  {
+    "revision": "a3c78bcbd606c27ce242",
+    "url": "/HelloAccount/js/chunk-vendors.920ea72d.js"
+  },
+  {
+    "revision": "f0fce8a1d94de3ce6d9839c05c88d85e",
+    "url": "/HelloAccount/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/HelloAccount/robots.txt"
+  }
+]);
